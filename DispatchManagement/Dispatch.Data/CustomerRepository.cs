f85f553d9c9716298f1214cd4b9958570e118249
rashisha <arularashi@gmail.com>
2016-07-31 22:00:39 -0700

    minor changes related to clean code

--- DispatchManagement/Dispatch.Data/CustomerRepository.cs
+++ DispatchManagement/Dispatch.Data/CustomerRepository.cs
@@ -1,10 +1,5 @@
 ﻿using Dispatch.Model;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dispatch.Data
 {
