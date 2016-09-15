edef6dad80515bdb97cc7286308b0b994dafabea
rashisha <arularashi@gmail.com>
2016-09-14 20:20:41 -0700

    Remove  test project

--- DispatchManagement/Dispatch.Test/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Dispatch.Test.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DispatchManagement/Dispatch.Test/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Dispatch.Test.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
